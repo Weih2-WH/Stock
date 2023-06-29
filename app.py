967b4d3461cc7fb68b5078f778190c7fbc3337a3
Weih2-WH <112489687+Weih2-WH@users.noreply.github.com>
2023-06-29 00:49:45 -0400

    Issue: Seperate the data pipeline and API request Auto upsert the history data into database

--- app.py
+++ app.py
@@ -5,10 +5,45 @@ from flask import Flask,request
 from datetime import datetime, timezone
 import yfinance as yf
 import numpy as np
+import time
+from flask_apscheduler import APScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 #read the .env variables
 load_dotenv()
-
 app = Flask(__name__)
+scheduler = APScheduler()
+
+
+"""
+    After the system started, which will load the 3 days price for each company and upsert into DB every 24 hrs.
+    Parameter: 
+                company : list
+                        The target companies.
+"""
+
+def upsert_history():
+    company = ['GOOG','MSFT','META','NOW']
+    his = yf.download(company, period='3d',group_by='ticker')
+    #handle the missing value
+    his.fillna(0)
+    for c in company:
+        data = his[c]
+        for ind in data.index:
+            date = datetime.strptime(str(ind)[:10], "%Y-%m-%d")
+            Open = float(data['Open'][ind])
+            high = float(data['High'][ind])
+            low = float(data['Low'][ind])
+            close = float(data['Close'][ind])
+            diff = high - low
+            volume = float(data['Volume'][ind])
+            with connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(DELETE_HISTORY_ROW,(c,date))
+                    cursor.execute(INSERT_HISTORY_TABLE,(c,date,Open,high,low,close,diff,volume))
+    return {"message": "3 days data insert success"}, 201
+
+
+
 #DB connect
 dburl = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(dburl)
@@ -59,3 +94,13 @@ def insert_history():
                 cursor.execute(DELETE_HISTORY_ROW,(company,date))
                 cursor.execute(INSERT_HISTORY_TABLE,(company,date,Open,high,low,close,diff,volume))
     return {"message": "5 days data insert success"}, 201
+
+
+
+if __name__ == '__main__':
+     # trigger every minute for testing the func
+     # To run on everyday 6pm -> replace the trigger CronTrigger(hour=18)
+    trigger = IntervalTrigger(minutes=1)
+    scheduler.add_job(id = "upsert_history",func=upsert_history,trigger=trigger,replace_existing=True) 
+    scheduler.start()
+    app.run(debug=False)
