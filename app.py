e2f9fc338283b1c4ec198c8b639fdfd2d239788f
Weih2-WH <112489687+Weih2-WH@users.noreply.github.com>
2023-06-30 00:21:33 -0400

    Create POST API: transac Store clients' transaction in to transaction table.

--- app.py
+++ app.py
@@ -4,21 +4,35 @@ from dotenv import load_dotenv
 from flask import Flask,request
 from datetime import datetime, timezone
 import yfinance as yf
-import numpy as np
-import time
+#import numpy as np
+#import time
 from flask_apscheduler import APScheduler
 from apscheduler.triggers.interval import IntervalTrigger
+
 #read the .env variables
 load_dotenv()
 app = Flask(__name__)
 scheduler = APScheduler()
 
+#DB connect
+dburl = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(dburl)
+
+#SQL
+CREATE_HISTORY_TABLE = """create table if not exists history(id SERIAL PRIMARY KEY, company TEXT, opendate TIMESTAMP, open real,
+                        high real, low real, close real,diff real, volume real);
+                        """
+INSERT_HISTORY_TABLE = "INSERT INTO history (company,opendate,open,high,low,close,diff,volume) VALUES (%s,%s,%s,%s,%s,%s,%s,%s);"
+DELETE_HISTORY_ROW = "DELETE FROM history where company = %s and opendate = %s;"
+SELECT_HISTORY_TABLE = ("""select company,opendate,open,high,low,close,diff,volume from history where company = %s and opendate = date(%s) """)
+CREATE_CLIENT_TABLE = """create table if not exists transaction(id SERIAL PRIMARY KEY, client INT, tDate TIMESTAMP, company TEXT,
+                        status char(1),price real, volume real); """
+INSERT_TRANSAC_TABLE = "INSERT INTO transaction (client,tDate,company,status,price,volume) VALUES (%s,%s,%s,%s,%s,%s);"
 
 """
     After the system started, which will load the 3 days price for each company and upsert into DB every 24 hrs.
     Parameter: 
-                company : list
-                        The target companies.
+                company : list : The target companies.
 """
 
 def upsert_history():
@@ -44,25 +58,17 @@ def upsert_history():
 
 
 
-#DB connect
-dburl = os.getenv("DATABASE_URL")
-connection = psycopg2.connect(dburl)
-
-#SQL
-CREATE_HISTORY_TABLE = """create table if not exists history(id SERIAL PRIMARY KEY, company TEXT, opendate TIMESTAMP, open real,
-                            high real, low real, close real,diff real, volume real);
-                        """
-INSERT_HISTORY_TABLE = "INSERT INTO history (company,opendate,open,high,low,close,diff,volume) VALUES (%s,%s,%s,%s,%s,%s,%s,%s);"
-DELETE_HISTORY_ROW = "DELETE FROM history where company = %s and opendate = %s;"
-SELECT_HISTORY_TABLE = ("""select company,opendate,open,high,low,close,diff,volume from history where company = %s and opendate = date(%s) """)
-
-
-
 @app.get("/")
 def home():
     return "Hello, Flask!"
 
-#get the company stock
+"""
+    Getting stock price
+    GET:
+    Parameter:
+            date : datetime
+            company : str
+"""
 @app.get("/api/get")
 def select_data():
     date = request.args.get("date")
@@ -73,27 +79,31 @@ def select_data():
             result = cursor.fetchone()
     return {"company":company,"date":date,"result":result,"message": "Select success."}, 201
 
-#insert stock history
-@app.post("/api/insert")
-def insert_history():
+"""
+    Record client's transaction (buy/sell stock).
+    POST:
+    Parameter:
+            client : int
+            tDate : datetime
+            company : str
+            status : char
+            price : decimal(8,2)
+            volume : int
+"""
+@app.post("/api/transac")
+def insert_transac():
     data = request.get_json()
+    client  = float(data['client'])
+    tDate  = datetime.strptime(data['tDate'], "%Y-%m-%d %H:%M:%S")
     company = data["company"]
-    tmp = yf.Ticker(company).history('5d')
-    #handle the missing value
-    tmp.fillna(0)
-    for ind in tmp.index:
-        date = datetime.strptime(str(ind)[:10], "%Y-%m-%d")
-        Open = float(tmp['Open'][ind])
-        high = float(tmp['High'][ind])
-        low = float(tmp['Low'][ind])
-        close = float(tmp['Close'][ind])
-        diff = high - low
-        volume = float(tmp['Volume'][ind])
-        with connection:
-            with connection.cursor() as cursor:
-                cursor.execute(DELETE_HISTORY_ROW,(company,date))
-                cursor.execute(INSERT_HISTORY_TABLE,(company,date,Open,high,low,close,diff,volume))
-    return {"message": "5 days data insert success"}, 201
+    status = data["status"]
+    price  = data["price"]
+    volume  = data["volume"]
+
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_TRANSAC_TABLE,(client,tDate,company,status,price,volume))
+    return {"message": "Transaction Successful"}, 201
 
 
 
